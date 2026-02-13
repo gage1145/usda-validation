02758a1263407351ceef037838855f3a12e70b80
gage1145 <140093724+gage1145@users.noreply.github.com>
2026-02-13 15:15:31 -0600

    I have no idea what I'm doing.

--- update_airtable.py
+++ update_airtable.py
@@ -1,13 +1,32 @@
 from dotenv import load_dotenv
 import os
 from pyairtable import Api
+from models import *
 
 
 load_dotenv()
 KEY = os.getenv('KEY')
 APP = os.getenv('APP')
-TBL = os.getenv('TBL')
 
 api = Api(KEY)
-table = api.table(APP, TBL)
-print(table.all())
+base = api.base(APP)
+
+
+# subject_fields = [
+#     {
+#         "name": "Sex",
+#         "type": "singleLineText",
+#     },
+#     {
+#         "name": "Animal ID",
+#         "type": "singleLineText",
+#     },
+# ]
+
+# subject_table = base.create_table(
+#     name="Subjects",
+#     fields=subject_fields,
+#     description="The animals used in the study."
+# )
+
+# print(f"Table '{subject_table.name}' created successfully with ID: {subject_table.id}")
